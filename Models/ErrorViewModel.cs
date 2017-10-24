1598abf8420ca7e9ae71d51540ae7c815e8dc4f5
hinst <hinstance@yandex.ru>
2017-10-24 21:26:36 +0300

    initial

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace bitb_reposhow_c.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
